ef6c96b2488447ffd34931e432448bb630df7bca
NS-Sheam <123sheamfeni@gmail.com>
2024-05-23 10:48:27 +0600

    fix: layout provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import Providers from "@/lib/Providers/Providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body className={inter.className}>
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </body>
+      </Providers>
     </html>
   );
 }
